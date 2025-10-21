060f75de1248359d553d143d09ef6bdc9a0efded
lexasss <oleg.spakov@tuni.fi>
2025-10-21 17:35:41 +0300

    minor changes

--- app/Models/HandLocation.cs
+++ app/Models/HandLocation.cs
@@ -9,11 +9,9 @@ public class Vector(double x, double y, double z)
     public double Y { get; set; } = y;
     public double Z { get; set; } = z;
     [JsonIgnore]
-    public double Magnitude => Math.Sqrt(X * X + Y * Y + Z * Z);
-    [JsonIgnore]
     public bool IsZero => X == 0 && Y == 0 && Z == 0;
     public static Vector Zero => new(0, 0, 0);
-    public static Vector From(ref readonly Leap.Vector v) => new(v.x, v.y, v.z);
+    public static Vector From(ref readonly Leap.Vector v) => new(Math.Round(v.x, 2), Math.Round(v.y, 2), Math.Round(v.z, 2));
     public static Vector operator /(Vector obj, double div) => new(obj.X / div, obj.Y / div, obj.Z / div);
     public static Vector operator *(Vector obj, double mul) => new(obj.X * mul, obj.Y * mul, obj.Z * mul);
 }
@@ -28,28 +26,5 @@ public class HandLocation(Vector palm, Vector thumb, Vector index, Vector middle
     public bool IsEmpty => Palm.IsZero && Thumb.IsZero && Index.IsZero && Middle.IsZero;
     public HandLocation() : this(Vector.Zero, Vector.Zero, Vector.Zero, Vector.Zero) { }
     public string AsJson() => JsonSerializer.Serialize(this);
-    public void CopyTo(HandLocation rhs)
-    {
-        rhs.Palm = Palm;
-        rhs.Thumb = Thumb;
-        rhs.Index = Index;
-        rhs.Middle = Middle;
-    }
-
     public static HandLocation Empty => new();
-    public static HandLocation FromJson(string json)
-    {
-        if (string.IsNullOrEmpty(json))
-            return Empty;
-
-        HandLocation result = Empty;
-
-        try
-        {
-            result = JsonSerializer.Deserialize<HandLocation>(json) ?? Empty;
-        }
-        catch { }
-
-        return result;
-    }
 }
